3b6786d8f06e0716dc6870daa16e85cc9e25183d
Anushek Prasal <anushekprasal@gmail.com>
2023-04-16 19:21:23 +0530

    Add Face Unlock support
    Signed-off-by: Anushek Prasal <anushekprasal@gmail.com>

--- config/havoc.mk
+++ config/havoc.mk
@@ -97,3 +97,17 @@ PRODUCT_PACKAGES += \
     libtextclassifier_annotator_universal_model \
     libtextclassifier_actions_suggestions_universal_model \
     libtextclassifier_lang_id_model
+
+# Face Unlock
+TARGET_FACE_UNLOCK_SUPPORTED ?= $(TARGET_SUPPORTS_64_BIT_APPS)
+ifeq ($(TARGET_FACE_UNLOCK_SUPPORTED),true)
+    PRODUCT_PACKAGES += \
+        ParanoidSense
+    PRODUCT_SYSTEM_EXT_PROPERTIES += \
+        ro.face.sense_service.enabled=true
+    PRODUCT_COPY_FILES += \
+        frameworks/native/data/etc/android.hardware.biometrics.face.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/android.hardware.biometrics.face.xml
+else
+    PRODUCT_PACKAGES += \
+        SettingsGoogleFutureFaceEnroll
+endif
